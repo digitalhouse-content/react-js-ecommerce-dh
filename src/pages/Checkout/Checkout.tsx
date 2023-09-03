9d2afb806c9b50ccfe34ab2ffbbfd437b682bd24
Humberto Rivero <humbertorivero9@gmail.com>
2023-09-03 17:58:28 -0300

    Add styles pages checkout and hidden cart in page checkout

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,11 +1,20 @@
-import { Table } from '../../components/ui/Table/Table'
+import {Table} from "../../components/ui/Table/Table";
+import styles from "./Checkout.module.css";
 
 const Checkout = () => {
-  return (
-    <div>
-      <Table />
-    </div>
-  )
-}
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>Checkout</h1>
+			<div className={styles.grid}>
+				<div>
+					<Table />
+        </div>
+        <div>
+          {/* Formulario Tarjeta */}
+        </div>
+			</div>
+		</div>
+	);
+};
 
-export default Checkout
+export default Checkout;

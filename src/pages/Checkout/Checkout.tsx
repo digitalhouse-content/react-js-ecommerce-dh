21d1edecb84228988670bc67288c544ff8670115
Humberto Rivero <humbertorivero9@gmail.com>
2023-09-03 19:52:52 -0300

    Add card credit

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,3 +1,4 @@
+import { CardCredit } from '../../components/ui/CardCredit/CardCredit';
 import {Table} from "../../components/ui/Table/Table";
 import styles from "./Checkout.module.css";
 
@@ -6,13 +7,14 @@ const Checkout = () => {
 		<div className={styles.container}>
 			<h1 className={styles.title}>Checkout</h1>
 			<div className={styles.grid}>
-				<div>
+				<div className={styles.tableContainer}>
 					<Table />
         </div>
         <div>
-          {/* Formulario Tarjeta */}
+          <CardCredit />
         </div>
-			</div>
+      </div>
+      <button className={styles.buyButton}>Buy Now</button>
 		</div>
 	);
 };

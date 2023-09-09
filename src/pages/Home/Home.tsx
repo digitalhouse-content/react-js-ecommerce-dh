433e6a02b38f6b8551dd5a895858b2496a920909
Humberto Rivero <humbertorivero9@gmail.com>
2023-09-09 12:39:24 -0300

    Add pagination

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,27 +1,51 @@
-import { useQuery } from 'react-query';
-import { Toaster } from 'sonner';
-import { CardProduct } from '../../components/ui/CardProduct';
-import { Hero } from '../../components/ui/Hero';
-import { getProducts } from '../../service';
+import {useQuery} from "react-query";
+import {Toaster} from "sonner";
+import {CardProduct} from "../../components/ui/CardProduct";
+import {Hero} from "../../components/ui/Hero";
+import {getProducts} from "../../service";
 import styles from "./Home.module.css";
+import {useState} from "react";
 
 const Home = () => {
+	const [page, setPage] = useState(1);
 
-  const {data, isLoading, error } = useQuery('products', getProducts)
+	const {data, isLoading, error} = useQuery(
+		["products", page],
+		() => getProducts(page),
+		{keepPreviousData: true}
+	);
 
-  return (
-    <>
-      <Hero />
-      <Toaster richColors />
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Something went wrong</p>}
-      <div className={styles.container}>
-        {data?.map((product) => (
-            <CardProduct key={product.tail} product={product} />
-          ))}
+	return (
+		<>
+			<Hero />
+			<Toaster richColors />
+			{isLoading && <p>Loading...</p>}
+			{error && <p>Something went wrong</p>}
+			<div className={styles.container}>
+				{data?.map((product) => (
+					<CardProduct key={product.tail} product={product} />
+				))}
       </div>
-    </>
-  )
-}
+      <div className={styles.paginationContainer}>
+        <button
+          onClick={() => setPage(page - 1)}
+          className={styles.paginationButton}
+          disabled={page === 1}
+        >
+          previus page
+        </button>
+        <div className={styles.paginationActive}>
+          <span>{page}</span>
+        </div>
+        <button
+          className={styles.paginationButton}
+          onClick={() => setPage(page + 1)}
+        >
+          next page
+        </button>
+      </div>
+		</>
+	);
+};
 
-export default Home
+export default Home;
--- src/service/products.service.ts
+++ src/service/products.service.ts
@@ -1,8 +1,8 @@
 import {Product} from '../interface'
 
-export const getProducts = async ():Promise<Product[]> => {
+export const getProducts = async (page = 0):Promise<Product[]> => {
   try {
-    const response = await fetch('http://localhost:3000/products')
+    const response = await fetch(`http://localhost:3000/products?_page=${page}&_limit=24`)
     
     if (response.ok) {
       const data = await response.json()
